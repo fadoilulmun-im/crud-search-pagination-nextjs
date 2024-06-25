5a15c06dd32f63d6d68da2c6ef580c8292117180
Fadoilul Munim <fadoilulmun.im2807@gmail.com>
2024-06-25 08:23:25 +0700

    fix: change route /contacts to /

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,35 @@
-export default function Home() {
+import { CreateButton } from "@/components/buttons";
+import ContactTable from "@/components/contact-table";
+import Pagination from "@/components/pagination";
+import Search from "@/components/search";
+import { TableSkeleton } from "@/components/skeleton";
+import { getContacPages } from "@/lib/data";
+import { Suspense } from "react";
+
+export default async function Contacts({
+  searchParams,
+}: {
+  searchParams?: {
+    search: string;
+    page: string;
+  };
+}) {
+  const search = searchParams?.search || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await getContacPages(search);
+  
   return (
-    <h1>Home Page</h1>
+    <div className="max-w-screen-md mx-auto mt-5">
+      <div className="flex items-center justify-between gap-1 mb-5">
+        <Search />
+        <CreateButton />
+      </div>
+      <Suspense key={search + currentPage} fallback={<TableSkeleton />}>
+        <ContactTable search={search} currentPage={currentPage} />
+      </Suspense>
+      <div className="flex justify-center mt-4">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
   );
 }
